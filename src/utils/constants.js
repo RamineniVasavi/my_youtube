3642a81e9d6e16b55814fb3717560fb588ca4aae
RamineniVasavi <vasaviramineni9@gmail.com>
2024-12-07 15:46:16 +0530

    Hide Api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const MY_GOOGLE_API_KEY="AIzaSyApsV34azHTogf1aOIEybkv0mfCIPtxfgI";
+export const MY_GOOGLE_API_KEY=process.env.REACT_APP_API_KEY;
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=20&chart=mostPopular&regionCode=US&key="+MY_GOOGLE_API_KEY;
 export const YOUTUBE_SEARCH_API="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=";
 export const LIVE_CHAT_COUNT=5;
